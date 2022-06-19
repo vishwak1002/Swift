e3ad2c6d31e5777aa7d77d4d4a241b82ad6f973d
Karnati vishwasai <37392493+vishwak1002@users.noreply.github.com>
2022-06-19 23:23:00 +0530

    Add files via upload

---
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    var player: AVAudioPlayer?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+//        print("Got Pressed")
+        playSound(title : sender.currentTitle!)
+        
+        print(sender.currentTitle);
+        sender.alpha = 0.5
+
+          //Code should execute after 0.2 second delay.
+          DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+              //Bring's sender's opacity back up to fully opaque.
+              sender.alpha = 1.0
+          }
+    }
+    
+    
+    
+
+    func playSound(title:String) {
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+               player = try! AVAudioPlayer(contentsOf: url!)
+        player!.play()
+
+    }
+
+}
+
